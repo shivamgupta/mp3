d612c07ad873508d7208e0df419d23bdb4265771
Shivam Gupta <shivamvmc@gmail.com>
2016-10-17 16:35:33 -0500

    Update app.js

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,20 @@
-var app = angular.module('mp3',['ngRoute']);
+var app = angular.module('mp3', ['ngRoute']);
 
-app.config(function ($routeProvider) {
-
-})
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/list", {
+        templateUrl : "../partials/list.html",
+        controller: 'MoviesListController'
+    })
+    .when("/details/:selectedRank", {
+        templateUrl : "../partials/details.html",
+        controller: 'MoviesDetailsController'
+    })
+    .when("/gallery", {
+        templateUrl : "../partials/gallery.html",
+        controller: 'MoviesGalleryController'
+    })
+    .otherwise({
+      redirectTo: '/list'
+    });
+});
